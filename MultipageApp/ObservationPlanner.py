031e52b4ff21d1915e393757d1d06c98c683930a
RC-26 <82444487+RC-26@users.noreply.github.com>
2026-06-29 20:25:57 +0800

    Update ObservationPlanner.py

--- MultipageApp/ObservationPlanner.py
+++ MultipageApp/ObservationPlanner.py
@@ -44,9 +44,9 @@ Render_Sidebar()
 
 ############################################################################
 
-st.title("Observational Scheduling Tool")
+st.title("Observation Scheduling Tool")
 
-st.markdown("This pipeline is for helping users plan their observations by extracting data from the **NASA Exoplanet Archive (NEA)** to predict the next transit events.  \nThe current iteration of this tool focuses on ground-based telescopes that are part of the **Skynet Robotic Telescope Network (SkyNet)**.") 
+st.markdown("This pipeline is for helping users plan and schedule their observations by extracting data from the **NASA Exoplanet Archive (NEA)** to predict the next transit events.  \nThe current iteration of this tool focuses on ground-based telescopes that are part of the **Skynet Robotic Telescope Network (SkyNet)**.") 
 
 st.divider(width = "stretch")
 
